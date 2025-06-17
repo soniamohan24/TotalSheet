72c7809d575d72fabce9dde8c5e05c0d70d23104
soniamohan24 <soniamohan.ie@gmail.com>
2025-06-18 00:12:43 +0100

    chnages telegram

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-#fghgfjfhjhjfeeeecccccc
+#fghgfj
 #deddbcdbjsm
 def main():
     """Run administrative tasks."""
