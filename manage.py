c553275c0c632461d8ddddb11d7ccfdfc3f3b6f7
soniamohan24 <soniamohan.ie@gmail.com>
2025-06-17 23:49:04 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 #fghgfjfhjhjf
-#ded
+#deddbcdbjsm
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "totalseats.settings")
